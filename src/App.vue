060591a80a73ffe8e80d502e3bd73d4afd628fe7
Martin Pruus <50612280268@tptlive.ee>
2025-01-14 11:37:36 +0200

    Added item list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
-
 <script setup>
-    let message = 'Hello TA23A'
+
+import { ref } from 'vue';
+
+
+let message = ref('');
+let items = ref(['Piim', 'Viin', 'Õlu', 'Krõpsud']);
+
+function addItem(){
+    if(message.value.trim() !== ''){
+        items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
+
+
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add Item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All Items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
