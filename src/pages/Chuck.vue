f6d7381742d086e97b1a8e7920a7ccdfe611b42d
Martin Pruus <martin.pruus@tptlive.ee>
2025-02-18 11:06:57 +0200

    Added chuck joke

--- src/pages/Chuck.vue
+++ src/pages/Chuck.vue
@@ -3,17 +3,12 @@ import { ref } from 'vue';
 
 let joke = ref(" ");
 
-function reqListener() {
-    let data = JSON.parse(this.responseText)
-    console.log(data.value);
+fetch("https://api.chucknorris.io/jokes/random").then(response => {
+    return response.json();
+}).then (data => {
+    console.log(data);
     joke.value = data.value;
-}
-
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open("GET", "http://api.chucknorries.io/jokes/random");
-req.send();
-
+});
 
 </script>
 
