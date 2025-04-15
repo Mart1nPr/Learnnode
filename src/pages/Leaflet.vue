f5fe1e9d2f0b5ada99c27e65df7dc7da430c0d76
Martin Pruus <martin.pruus@tptlive.ee>
2025-04-15 11:35:41 +0300

    Add Leaflet

---
+++ src/pages/Leaflet.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+import LeafletMap from '../components/LeafletMap.vue';
+
+let center = ref([59.42686, 24.74353]);
+let zoom = ref(19);
+
+</script>
+<template>
+    <button class="button is-primary" @click="center = [59.42686, 24.74353]">Home</button>
+    <input type="range" min="1" max="19" v-model="zoom">
+    <LeafletMap :center="center" :zoom="zoom"></LeafletMap>
+</template>
