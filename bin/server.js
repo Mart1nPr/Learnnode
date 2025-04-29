087edd870bac004efad9dd6e6f8c88d945719dcc
Martin Pruus <martin.pruus@tptlive.ee>
2025-04-29 10:39:38 +0300

    Add longpolling

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 let messages = [];
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,10 +20,21 @@ app.get('/', (req, res) => {
     res.json(filteredMessages);
 });
 
+app.get('/longpoll', async (req, res) => {
+    let date = req.query.date ?? null;
+    let filteredMessages;
+    do {
+        await sleep(1000);
+        filteredMessages = messages.filter(message => message.date > new Date(date));
+    } while(filteredMessages.length == 0);
+    res.json(filteredMessages);
+});
+
 app.post('/', (req, res) => {
     messages.push({ message: req.body.message, date: new Date() });
     res.json(req.body);
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -8,15 +8,17 @@ let input = ref('');
 let res = await axios.get('http://localhost:3000');
 messages.value = res.data;
 
-setInterval(async () => {
-    let date = messages.value[messages.value.lenght-1]?.date ?? null;
-    let res = await axios.get('http://localhost:3000', {
+function longpoll() {
+    let date = messages.value[messages.value.lenght - 1]?.date ?? null;
+    axios.get('http://localhost:3000/longpoll', {
         params: {
             date: new Date(date)
         }
+    }).then(res => {
+        messages.value.push(...res.data);
+        longpoll();
     });
-    messages.value.push(...res.data);
-}, 1000);
+}
 
 async function send() {
     let res = await axios.post('http://localhost:3000', {
