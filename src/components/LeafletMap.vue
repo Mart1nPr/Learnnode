80dced545741d482d8923cdd6107fb236e233994
Martin Pruus <martin.pruus@tptlive.ee>
2025-05-31 19:08:55 +0300

    Add home with polygon to leaflet

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,34 +1,54 @@
 <script setup>
-import L from 'leaflet';
-import { onMounted, useId, watch } from 'vue';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet/dist/images/marker-icon-2x.png';
-import 'leaflet/dist/images/marker-shadow.png';
+import L from "leaflet";
+import { onMounted, useId, watch } from "vue";
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-icon-2x.png";
+import "leaflet/dist/images/marker-shadow.png";
 
-const {center, zoom} = defineProps(['center', 'zoom']);
-const id = 'map-' + useId();
-let map; 
+const { center, zoom } = defineProps(["center", "zoom"]);
+const id = "map-" + useId();
+let map;
 
 onMounted(() => {
-    map = L.map(id).setView(center, zoom);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    var marker = L.marker([59.42686, 24.74353]).addTo(map);
+  map = L.map(id).setView(center, zoom);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  var marker = L.marker([59.42686, 24.74353]).addTo(map);
+  var marker = L.marker([59.415851, 24.799275]).addTo(map);
+
+  var polygon = L.polygon([
+    [59.415524, 24.798830],
+    [59.417379, 24.798878],
+    [59.417376, 24.799823],
+    [59.416539, 24.799817],
+    [59.416474, 24.803604],
+    [59.416285, 24.803615],
+    [59.416250, 24.799806],
+    [59.415516, 24.799790],
+  ]).addTo(map);
 });
-watch(() => center, (center, oldCenter) => {
+watch(
+  () => center,
+  (center, oldCenter) => {
     map.panTo(center);
-});
-watch(() => zoom, (zoom) => {
+  }
+);
+watch(
+  () => zoom,
+  (zoom) => {
     map.setZoom(zoom);
-});
+  }
+);
 </script>
 <template>
-    <div :id="id"></div>
+  <div :id="id"></div>
 </template>
 <style scoped>
 div {
-    height: 90vh;
+  height: 90vh;
 }
 </style>
--- src/pages/Leaflet.vue
+++ src/pages/Leaflet.vue
@@ -2,12 +2,13 @@
 import { ref } from 'vue';
 import LeafletMap from '../components/LeafletMap.vue';
 
-let center = ref([59.42686, 24.74353]);
+let center = ref([59.437216, 24.745369]);
 let zoom = ref(19);
 
 </script>
 <template>
-    <button class="button is-primary" @click="center = [59.42686, 24.74353]">Home</button>
+    <button class="button is-primary" @click="center = [59.42686, 24.74353]">School</button>
+    <button class="button is-primary" @click="center = [59.415851, 24.799275]">Home</button>
     <input type="range" min="1" max="19" v-model="zoom">
     <LeafletMap :center="center" :zoom="zoom"></LeafletMap>
 </template>
